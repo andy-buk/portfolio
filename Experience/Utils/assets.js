30e863fecc1d028de540ab2ab43600c535b79ef2
andy b <andy.bukovcan@gmail.com>
2023-05-08 12:16:20 -0400

    2023-05-08 12:16 PM

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    name: "room",
+    type: "glbModel",
+    path: "/models/portfolio.glb"
+  }
+  // use this if a video texture is needed
+  //,
+  // {
+  //   name: "screen",
+  //   type: "videoTexture",
+  //   path: "path here"
+  // }
+]
---
+++ Experience/World/Room.js
@@ -0,0 +1,80 @@
+import * as THREE from "three";
+import Experience from "../Experience.js";
+import GSAP from "gsap";
+
+export default class Room {
+    constructor() {
+        this.experience = new Experience();
+        this.scene = this.experience.scene;
+        this.resources = this.experience.resources;
+        this.time = this.experience.time;
+        this.room = this.resources.items.room;
+        this.actualRoom = this.room.scene;  
+        
+        this.lerp = {
+            current: 0,
+            target: 0,
+            ease: 0.1,
+          };
+        
+        this.setModel();
+        this.setAnimation();
+        this.onMouseMove();
+    }
+
+    setModel() {
+        this.actualRoom.children.forEach((child) => {
+            child.castShadow = true;
+            child.receiveShadow = true;
+
+            if (child instanceof THREE.Group) {
+                child.children.forEach((groupchild) => {
+                    groupchild.castShadow = true;
+                    groupchild.receiveShadow = true;
+                })
+            }
+        })
+
+        // can also update any specific textures here
+
+        this.scene.add(this.actualRoom);
+        this.actualRoom.scale.set(0.5, 0.5, 0.5);
+        // rotation:
+        // this.actualRoom.rotation.y = Math.PI;
+    }
+
+    setAnimation() {
+        this.mixer = new THREE.AnimationMixer(this.actualRoom);
+
+        // console.log(this.room);
+        // fix the animation number in the future
+        this.chair = this.mixer.clipAction(this.room.animations[7]);
+        this.chair.play();
+    }
+
+    onMouseMove() {
+        console.log("called");
+        window.addEventListener("mousemove", (e) => {
+            console.log("inside");
+            this.rotation =
+                ((e.clientX - window.innerWidth / 2) * 2) / window.innerWidth;
+            this.lerp.target = this.rotation * 0.05;
+        });
+    }
+
+    resize() {
+
+    }
+
+    update() {
+        this.lerp.current = GSAP.utils.interpolate(
+            this.lerp.current,
+            this.lerp.target,
+            this.lerp.ease
+        );
+
+        this.actualRoom.rotation.y = this.lerp.current;
+
+        this.mixer.update(this.time.delta * 0.0009);
+    }
+}
