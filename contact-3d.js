70acbd776ac3f47e5c3607ab3ff8170e9d218489
andy b <andy.bukovcan@gmail.com>
2023-05-24 22:18:32 -0400

    Updated 2023-05-24

--- contact-3d.js
+++ contact-3d.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    camera.position.z = 1.4;
+    camera.position.z = 1.5;
 
     const canvas = document.getElementById('contact3d');
     const renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true });
