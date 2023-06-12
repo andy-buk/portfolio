cc43896523de1e3c68f29cd57330ace6b5361eb7
andy b <andy.bukovcan@gmail.com>
2023-06-12 19:53:48 -0400

    reduced load time

--- Experience/Utils/assets.js
+++ Experience/Utils/assets.js
@@ -2,6 +2,6 @@ export default [
   {
     name: "room",
     type: "glbModel",
-    path: "/models/final3-v1.glb"
+    path: "/models/final3-v1-transformed.glb"
   },
 ]
--- typing.js
+++ typing.js
@@ -1,13 +1,13 @@
 var typed = new Typed(".auto-type", {
   strings: [
-    "unraveling the mysteries of the code universe",
-    "venturing into the depths of knowledge through studying",
-    "entering boundless realms of fantasy with gaming",
-    "embarking on rejuvenating journeys to dreamland",
-    "immersing into diverse realities with television",
-    "savoring the symphony of flavors in every meal",
+    "deciphering code mysteries",
+    "plunging into knowledge",
+    "navigating gaming realms",
+    "journeying through dreams",
+    "exploring TV realities",
+    "feasting on flavor symphonies",
   ],
-  typeSpeed: 70,
-  backSpeed: 70,
+  typeSpeed: 100,
+  backSpeed: 100,
   loop: true,
 });
